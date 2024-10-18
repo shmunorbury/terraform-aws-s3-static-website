19a8c47d0f6b0b4b920b7509ea2c8098396511b8
samuel norbury <samuel@norbury.nl>
2024-10-18 22:26:31 +0200

    add ipv6 route

--- route53.tf
+++ route53.tf
@@ -16,6 +16,29 @@ resource "aws_route53_record" "root-a" {
   }
 }
 
+resource "aws_route53_record" "root-aaaa" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = var.domain_name
+  type    = "AAAA"
+
+  alias {
+    name                   = aws_cloudfront_distribution.root_s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.root_s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "www-aaaa" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "www.${var.domain_name}"
+  type    = "AAAA"
+
+  alias {
+    name                   = aws_cloudfront_distribution.root_s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.root_s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
 resource "aws_route53_record" "www-a" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "www.${var.domain_name}"
