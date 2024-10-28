857cb04f54d7ec2d672189ef75e37a1cc5019f6b
samuel norbury <samuel@norbury.nl>
2024-10-28 16:32:20 +0100

    add default web-acl

--- variables.tf
+++ variables.tf
@@ -11,3 +11,9 @@ variable "bucket_name" {
 variable "common_tags" {
   description = "Common tags you want applied to all components."
 }
+
+variable "use_waf" {
+  description = "Whether or not to enable the simplest, cheapest AWS WAF offering for security/ddos protection."
+  default     = false
+  type        = bool
+}
