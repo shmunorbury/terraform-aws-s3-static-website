36b5840a21bd6fce172cc8b250561d543ffd7e2c
samuel norbury <samuel@norbury.nl>
2025-01-01 16:14:21 +0100

    fix: use correct versions etc

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,4 +1,5 @@
 resource "aws_cloudwatch_metric_alarm" "email_topic" {
+  provider = aws.acm_provider
   count                     = var.admin_email == "" ? 0 : 1
   alarm_name                = "high-traffic-${var.domain_name}"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
@@ -6,7 +7,7 @@ resource "aws_cloudwatch_metric_alarm" "email_topic" {
   metric_name               = "CPUUtilization"
   namespace                 = "AWS/EC2"
   period                    = 60
-  statistic                 = "SUM"
+  statistic                 = "Sum"
   threshold                 = var.request_limit
   alarm_description         = "This will fire an alert when the number of requests is more than ${var.request_limit} per minute for 5 consecutive minutes."
   insufficient_data_actions = []
@@ -16,10 +17,12 @@ resource "aws_cloudwatch_metric_alarm" "email_topic" {
 }
 
 resource "aws_sns_topic" "sns" {
+  provider = aws.acm_provider
   name = "email_topic_for_admin"
 }
 
-resource "sns_topic_subscription" "email" {
+resource "aws_sns_topic_subscription" "email" {
+  provider = aws.acm_provider
   count     = var.admin_email == "" ? 0 : 1
   endpoint  = var.admin_email
   protocol  = "email"
--- examples/main.tf
+++ examples/main.tf
@@ -2,6 +2,7 @@ module "s3_website" {
   source      = "../"
   bucket_name = "example.org"
   domain_name = "example.org"
+  admin_email = "test@examples.com"
   common_tags = {
     x = "y"
   }
