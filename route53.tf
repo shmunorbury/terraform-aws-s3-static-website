c607064a101c555471f558230e6a163d201978f9
samuel norbury <samuel@norbury.nl>
2025-01-12 10:02:07 +0100

    feat: Add DNSSEC hosted zone signing

--- route53.tf
+++ route53.tf
@@ -69,3 +69,54 @@ resource "aws_route53_record" "cert_validation" {
   type            = each.value.type
   zone_id         = each.value.zone_id
 }
+
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_kms_key" "this" {
+  provider = aws.acm_provider
+  customer_master_key_spec = "ECC_NIST_P256"
+  deletion_window_in_days  = 7
+  key_usage                = "SIGN_VERIFY"
+  policy = jsonencode({
+    Statement = [
+      {
+        Action = [
+          "kms:DescribeKey",
+          "kms:GetPublicKey",
+          "kms:Sign",
+          "kms:Verify",
+        ],
+        Effect = "Allow"
+        Principal = {
+          Service = "dnssec-route53.amazonaws.com"
+        }
+        Resource = "*"
+        Sid      = "Allow Route 53 DNSSEC Service",
+      },
+      {
+        Action = "kms:*"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Resource = "*"
+        Sid      = "Enable IAM User Permissions"
+      },
+    ]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_route53_key_signing_key" "this" {
+  hosted_zone_id             = aws_route53_zone.main.id
+  key_management_service_arn = aws_kms_key.this.arn
+  name                       = "${var.domain_name}_dnssec_signing_key"
+}
+
+resource "aws_route53_hosted_zone_dnssec" "this" {
+  depends_on = [
+    aws_route53_key_signing_key.this
+  ]
+  hosted_zone_id = aws_route53_key_signing_key.this.hosted_zone_id
+}
